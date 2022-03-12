09f5e94cd8937a414294c9476163276eb1f31dd2
bitwirealbert <bitwire.albert@gmail.com>
2022-03-12 20:01:34 +0300

    New DSA site

---
+++ assets/js/news.js
@@ -0,0 +1,31 @@
+(function($) {
+
+	
+	var carousel = function() {
+		$('.featured-carousel').owlCarousel({
+	    loop:true,
+	    autoplay: true,
+	    margin:30,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    nav:true,
+	    dots: true,
+	    autoplayHoverPause: false,
+	    items: 3,
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:2
+	      },
+	      1000:{
+	        items:3
+	      }
+	    }
+		});
+
+	};
+	carousel();
+
+})(jQuery);
